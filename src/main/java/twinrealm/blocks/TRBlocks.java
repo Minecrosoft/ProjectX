9c37375006fe7ccf4a89cdf7af72d57e184f4676
Ivorius <lukastenbrink@googlemail.com>
2014-09-15 19:52:07 +0200

    Add TRBlocks and TRItems

---
+++ src/main/java/twinrealm/blocks/TRBlocks.java
@@ -0,0 +1,8 @@
+package twinrealm.blocks;
+
+/**
+ * Created by lukas on 15.09.14.
+ */
+public final class TRBlocks
+{
+}
---
+++ src/main/java/twinrealm/items/TRItems.java
@@ -0,0 +1,9 @@
+package twinrealm.items;
+
+/**
+ * Created by lukas on 15.09.14.
+ */
+public final class TRItems
+{
+
+}
