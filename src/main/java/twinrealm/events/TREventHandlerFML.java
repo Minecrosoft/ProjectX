2cf738ae457fdb5f1a704ceeaf58d6e20016a5f5
Ivorius <lukastenbrink@googlemail.com>
2014-09-16 09:40:25 +0200

    Make config interprete and save on changes

--- src/main/java/twinrealm/events/TREventHandlerFML.java
+++ src/main/java/twinrealm/events/TREventHandlerFML.java
@@ -1,6 +1,10 @@
 package twinrealm.events;
 
+import cpw.mods.fml.client.event.ConfigChangedEvent;
 import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import twinrealm.TwinRealm;
+import twinrealm.configuration.TRConfig;
 
 /**
  * Created by lukas on 15.09.14.
@@ -10,5 +14,17 @@ public class TREventHandlerFML
     public void register()
     {
         FMLCommonHandler.instance().bus().register(this);
+    }
+
+    @SubscribeEvent
+    public void onConfigChanged(ConfigChangedEvent event)
+    {
+        if (event instanceof ConfigChangedEvent.OnConfigChangedEvent && event.modID.equals(TwinRealm.MODID))
+        {
+            TRConfig.loadConfig(event.configID);
+
+            if (TwinRealm.config.hasChanged())
+                TwinRealm.config.save();
+        }
     }
 }
